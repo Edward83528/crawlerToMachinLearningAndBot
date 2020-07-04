526c9b6594c5c0661d4b4321cbc7415aba945065
Edward83528 <u0151051@gmail.com>
2020-07-04 21:27:21 +0800

    更新說明

--- 05.mongo/mongo01/mongo01.py
+++ 05.mongo/mongo01/mongo01.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pymongo as mg
 
-client = mg.MongoClient('127.0.0.1:27017')
+client = mg.MongoClient('127.0.0.1:27017') # mongo預設port 27017
 db = client['local']
 result = db['startup_log'].find({})
 lst  =  list(result)
--- 05.mongo/mongo02/mongo02.py
+++ 05.mongo/mongo02/mongo02.py
@@ -3,17 +3,17 @@ import pymongo as mg
 import datetime
 
 client = mg.MongoClient('127.0.0.1:27017')
-db = client['uuutest'] #DBName: uuutest
+db = client['mydb'] #DBName: mydb
 
-#post is collection name
-result = db.post.insert_one(
+#mycollection is collection name
+result = db.mycollection.insert_one(
 {
     "title": 'Headline of the day',
     "body":'blah blah blah...',
     "datetime":datetime.datetime.now()
 } )
 
-result = db['post'].find({})
+result = db['mycollection'].find({})
 lst  =  list(result)
 
 for x in lst:
